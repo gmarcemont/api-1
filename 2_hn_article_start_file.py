0f5804e95527a7f20f7bdcec172d69dcea29ba86
Grayson <Grayson_Arcemont1@baylor.edu>
2023-04-05 13:51:00 -0500

    hn2.json complete

--- 2_hn_article_start_file.py
+++ 2_hn_article_start_file.py
@@ -2,8 +2,33 @@ import requests
 import json
 
 # Make an API call, and store the response.
-url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
 
 # Explore the structure of the data.
+outfile = open('hn.json', 'w')
+
+json.dump(r.json(), outfile, indent=4)
+
+submission_ids = r.json()
+
+url = 'https://hacker-news.firebaseio.com/v0/item/35457341.json'
+r = requests.get(url)
+
+outfile = open('hn2.json', 'w')
+json.dump(r.json(), outfile)
+
+for id in submission_ids[:21]:
+    url = f"https://hacker-news.firebaseio.com/v0/item/{id}.json"
+    r = requests.get(url)
+    response_dict = r.json()
+
+    print(f'title: {response_dict["title"]}')
+    print(f'link: http://news.ycombinator.com/item?id={id}')
+    try:
+        print(f"comments: {response_dict['descendants']}")
+    except:
+        print(f"comments: 0")
+
+    input()
